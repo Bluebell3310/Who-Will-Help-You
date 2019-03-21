2943b884678bcba365aa17d882cd2fcd76ce7d6f
trkuan <tianruei.kuan@gmail.com>
2019-03-21 23:40:03 +0800

    splash page

---
+++ why/static/js/base.js
@@ -0,0 +1,7 @@
+//smooth scrolling
+$(function() {
+	$('a[href*="#"]').on('click', function(e) {
+		e.preventDefault()
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 'slow', 'swing')
+	})
+})
